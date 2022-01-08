0329a32a5df7dd99403937ea403bbf890a74fe0a
mahbobe.9495@gmail.com <mahbobe.9495@gmail.com>
2022-01-08 15:31:30 +0330

    edit admin scope

---
+++ app/Helpers/ScopeHelper.php
@@ -0,0 +1,37 @@
+<?php
+
+
+namespace App\Helpers;
+
+
+class ScopeHelper
+{
+    const DELIMITER = ' ';
+    const ADMIN_KEY = 'admin';
+
+    public static function isAdmin($scopes)
+    {
+        if (
+        ScopeHelper::inScope($scopes, self::ADMIN_KEY)
+        ) {
+            return true;
+        }
+
+        return false;
+    }
+    public static function inScope($scopes, $needle)
+    {
+        if (is_string($scopes)) {
+            $scopes = explode(self::DELIMITER, $scopes);
+            if (in_array($needle, $scopes)) {
+                return true;
+            }
+        } elseif (is_array($scopes)) {
+            if (in_array($needle, $scopes)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
